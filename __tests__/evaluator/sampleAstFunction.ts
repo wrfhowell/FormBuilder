5d5dcaaf372a42c667a64eb21126c842202eec94
Mohan Grewal <mohangrewal24@gmail.com>
2024-02-13 17:10:25 -0800

    Sample AST Refactored

--- __tests__/evaluator/sampleAstFunction.ts
+++ __tests__/evaluator/sampleAstFunction.ts
@@ -15,6 +15,8 @@ import {FunctionCustom} from "../../src/AST/Nodes/FunctionCustom";
 import {UnscopedExpression} from "../../src/AST/Nodes/UnscopedExpression";
 import {Function_Body} from "../../src/AST/Nodes/Function_Body";
 import {Function_Call} from "../../src/AST/Nodes/Function_Call";
+import {BooleanExpression} from "../../src/AST/Nodes/BooleanExpression";
+import {ScopedExpression} from "../../src/AST/Nodes/ScopedExpression";
 
 //create a parameters for a function that returns the value of 10 + 10!"
 const mathematicsParameters = ["10", " 10"];
@@ -29,16 +31,59 @@ const mathematicsFunctionBody = new Function_Body(
     [mathematicsStatement],
     mathematicsFunctionReturnValue);
 const mathematicsFunctionName = new VariableName("MathematicsFunction");
-
 const mathematicsFunction = new FunctionCustom(mathematicsFunctionName, mathematicsParameters, mathematicsFunctionBody);
-
 
 //TODO: Decided to add this but unsure what the point of it is after building the AST? Ask Rodrigo, as
 // I just converted what he wrote.
 const mathematicsFunctionCall = new Function_Call(mathematicsFunctionName, mathematicsParameters);
 
 
-const functions = new Functions_Array([mathematicsFunction]);
+//create a scoped expression
+
+//returns the VALUE of TRUE, which evaluator will determine
+const booleanNumExpression = new Expression(
+    new UnscopedExpression(
+        new BooleanExpression("10 > 4"),
+        "10 > 4")
+);
+
+//this just returns the value of FALSE, which the evaluator will determine
+const booleanStringExpression = new Expression(
+    new UnscopedExpression(
+        new BooleanExpression("Goodbye == Hello"),
+        "Goodbye == Hello")
+);
+
+//create a string expression
+const stringExpression = new UnscopedExpression(
+        new StringExpression("This is a string"),
+        "This is a string"
+    );
+
+//create new scoped Expression
+// StringExpression == BooleanNumExpression != BooleanStringExpression
+// This should be evaluated into a single string to which the Evaluator will return true.
+const expressionFunctionReturnValue = new Expression(new ScopedExpression(
+    stringExpression,
+    {
+        "==": booleanNumExpression,
+        "!=": booleanStringExpression
+    }
+    )
+);
+
+const expressionFunctionBody = new Function_Body(
+    [],
+    expressionFunctionReturnValue);
+const expressionFunctionName = new VariableName("ExpressionFunction");
+const expressionFunction = new FunctionCustom(expressionFunctionName, [], expressionFunctionBody);
+
+//TODO: Decided to add this but unsure what the point of it is after building the AST? Ask Rodrigo, as
+// I just converted what he wrote.
+const expressionFunctionCall = new Function_Call(expressionFunctionName, []);
+
+
+const functions = new Functions_Array([mathematicsFunction, expressionFunction]);
 
 const globalVariables = new VariablesArray([new Variable("score", 0)]);
 
@@ -67,8 +112,19 @@ const question = new Question(
     <string> mathematicsFunction.getFunctionBody().getFunctionReturnValue(),
     undefined
 );
-
-const questionsArray = new Question_Array([question]);
+const scopedExpressionQuestionExample = new Question(
+    "randomExample",
+    undefined,
+    "This is a scoped expression example",
+    new ArrayCustom([]),
+    undefined,
+    undefined,
+    undefined,
+    true,
+    expressionFunctionReturnValue,
+    undefined
+);
+const questionsArray = new Question_Array([question, scopedExpressionQuestionExample]);
 
 const goTo = "end";
 
